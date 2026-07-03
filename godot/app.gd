0bf80b6927cf6a4de0a41b040f915426517af507
adleu <audia.adel@gmail.com>
2026-07-04 01:07:35 +0200

    window movable with mouse

--- godot/app.gd
+++ godot/app.gd
@@ -13,6 +13,10 @@ signal increment
 const move_speed 	= 10
 var i				= 0
 
+## window drag
+var mouse_pressed 	: bool = false
+var offset			: Vector2i
+
 func _ready() -> void:
 	label.text = str(i)
 
@@ -36,6 +40,7 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
 		right_click.visible = true
 		label.visible 		= false
+		mouse_pressed 		= false
 
 	if event is InputEventKey and event.pressed:	
 		match event.keycode:
@@ -47,6 +52,23 @@ func _input(event: InputEvent) -> void:
 				get_window().position.x -= move_speed
 			KEY_RIGHT:
 				get_window().position.x += move_speed
+
+		clamp_window_to_screen(get_window().size.x - 64)
+	
+	if right_click.visible:
+		return
+
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT: 
+		if event.pressed:
+			mouse_pressed 	= true
+			offset			= get_window().position - DisplayServer.mouse_get_position()
+		else :
+			mouse_pressed = false
+	
+	if mouse_pressed and event is InputEventMouseMotion :
+		get_window().position += Vector2i(event.relative)
+		clamp_window_to_screen(get_window().size.x - 64)
+
 
 func change_i(add : bool):
 	i += 1 if add else -1
@@ -75,3 +97,18 @@ func set_to_last_value() :
 
 func update_text() -> void :
 	label.text = str(i)
+
+func clamp_window_to_screen(_offset : float = 0):
+	var window 			= get_window()
+	var screen_size 	= DisplayServer.screen_get_size()
+	var screen_index 	= DisplayServer.window_get_current_screen()
+	var screen_pos 		= DisplayServer.screen_get_position(screen_index)
+	
+	var window_pos 	= window.position
+	var window_size = window.size
+	
+	var new_x = clamp(window_pos.x, screen_pos.x - _offset, screen_pos.x + screen_size.x - window_size.x + _offset)
+	var new_y = clamp(window_pos.y, screen_pos.y - _offset, screen_pos.y + screen_size.y - window_size.y + _offset)
+	
+	if new_x != window_pos.x || new_y != window_pos.y:
+		window.position = Vector2i(new_x, new_y)
